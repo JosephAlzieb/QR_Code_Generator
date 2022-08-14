029c0d2d85dc21f09e93deab86ca168b9379925a
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-08-14 21:22:36 +0200

    add some colors

---
+++ lib/colors.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor =  Color(0xFF222555);
+  static Color textInputColor = const Color(0xFF404040);
+  static Color accentColor = const Color(0xFF4c90d2);
+}
